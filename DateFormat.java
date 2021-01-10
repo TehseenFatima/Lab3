4a7ef2c60d77f3a50029bfc2beaaf8f93f8a9635
TehseenFatima <77046136+TehseenFatima@users.noreply.github.com>
2021-01-10 20:14:21 +0530

    Add files via upload

---
+++ DateFormat.java
@@ -0,0 +1,86 @@
+/*
+ * creating class DateFormat
+ */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateFormat {
+
+	public static void main(String[] args) {
+		/*
+		 * creating scanner object
+		 */
+		Scanner sc = new Scanner(System.in);
+
+		/*
+		 * scanning input from the user in the form of yyyy/MM/dd format
+		 */
+		System.out.println("Enter the date in yyyy/MM/dd format :");
+		String userDate = sc.nextLine();
+
+		/*
+		 * taking the system date in the format yyyy/MM/dd
+		 */
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDateTime now = LocalDateTime.now();
+		/*
+		 * storing the value in the string variable systemDate
+		 */
+		String systemDate = dtf.format(now);
+
+		/*
+		 * displaying the result
+		 */
+		System.out.println(systemDate + "\n");
+
+		/*
+		 * calling the method getDuration
+		 * 
+		 * @param userDtae, systemDate
+		 */
+		String result = getDuration(userDate, systemDate);
+		String date[] = result.split("-");
+
+		/*
+		 * Displaying the diference in years, months, days
+		 */
+		System.out.println("The difference in Years: " + date[0]);
+		System.out.println("The difference in Months: " + date[1]);
+		System.out.println("The difference in Days: " + date[2]);
+		/*
+		 * close the scanner
+		 */
+		sc.close();
+	}
+
+	/*
+	 * To accept date and print the duration in days, months and years with regards
+	 * to current system date.
+	 */
+	private static String getDuration(String userDate, String systemDate) {
+		/*
+		 * creating string buffer object
+		 */
+		StringBuffer sb = new StringBuffer();
+
+		/*
+		 * Splitting userDate and systemDate using "/"
+		 */
+		String user[] = userDate.split("/");
+		String system[] = systemDate.split("/");
+
+		/*
+		 * Appending the difference to StringBuffer
+		 */
+		for (int i = 0; i < user.length; i++) {
+			int difference = Math.abs((Integer.parseInt(user[i]) - Integer.parseInt(system[i])));
+			sb.append(difference + "-");
+		}
+		/*
+		 * return the result by converting from stringBuffer to string
+		 */
+		return sb.toString();
+	}
+
+}
---
+++ MirrorImage.java
@@ -0,0 +1,62 @@
+
+import java.util.*;
+
+/*
+ * creating class for MirrorImage
+ */
+public class MirrorImage {
+
+	public static void main(String[] args) {
+		/*
+		 * creating class for scanner object
+		 */
+		Scanner sc = new Scanner(System.in);
+
+		/*
+		 * Scanning the input from User
+		 */
+		System.out.println("Enter the String for Mirror Image :");
+		String string = sc.nextLine();
+
+		/*
+		 * Calling getImage method
+		 * 
+		 * @param string
+		 */
+		String reverse = getImage(string);
+
+		/*
+		 * Displaying the result
+		 */
+		System.out.println("The final Mirror Image is :");
+		System.out.println(string + "|" + reverse);
+		/*
+		 * closing the scanner
+		 */
+		sc.close();
+
+	}
+
+	/*
+	 * Generate the mirror image of a String and add it to the existing string.
+	 */
+	private static String getImage(String string) {
+		/*
+		 *creating the strinBuffer object
+		 */
+		StringBuffer sb = new StringBuffer(string);
+		/*
+		 * String reverse
+		 */
+		sb.reverse();
+		/*
+		 * converting to String return result;
+		 */
+		String result = sb.toString();
+		/*
+		 * returning the result
+		 */
+		return result;
+	}
+
+}   
